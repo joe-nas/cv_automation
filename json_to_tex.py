6263a91f18bfee4b03044f2c28065cf8823e3f0e
Jonas Falck <pintac1984@gmail.com>
2024-04-16 11:10:40 +0200

    Add new files and templates for personal info, skills, education, software, experience, and about sections

---
+++ json_to_tex.py
@@ -0,0 +1,34 @@
+import json
+from jinja2 import Template,Environment, FileSystemLoader
+
+def open_json_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def save_tex_file(file_path, tex_string):
+    with open(file_path, 'w') as file:
+        file.write(tex_string)
+    return
+
+def parse_json(json_string):
+    data = json.loads(json_string)
+    return data
+
+skills = open_json_file('data/skills.json')
+experience = open_json_file('data/experience.json')
+software = open_json_file('data/projects.json')
+
+def render_to_tex(template_path, data):
+    with open(template_path, 'r') as file:
+        template = Template(file.read())
+    return template.render(data)
+
+skills_output = render_to_tex('j2/skills.tex.j2', data = {'data' : skills['content']})
+save_tex_file('output/skills.tex', skills_output)
+
+experience_output = render_to_tex('j2/experience.tex.j2', data = {'data' : experience['content']})
+save_tex_file('output/experience.tex', experience_output)
+
+software_output = render_to_tex('j2/software.tex.j2', data = {'data' : software['content']})
+save_tex_file('output/projects.tex', software_output)
