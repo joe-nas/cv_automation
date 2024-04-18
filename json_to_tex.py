ecac02915b2efbe41eaa409e2b2eb1446f2221d4
joe-nas <pintac1984@gmail.com>
2024-04-18 12:26:09 +0200

    Refactor json_to_tex.py to use process_date function for DRY

--- json_to_tex.py
+++ json_to_tex.py
@@ -2,44 +2,33 @@ import json
 from jinja2 import Template
 
 def open_json_file(file_path):
+    """Open a json file and return the data as a dictionary."""
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-def save_tex_file(file_path, tex_string):
-    with open(file_path, 'w') as file:
-        file.write(tex_string)
-    return
-
-def parse_json(json_string):
-    data = json.loads(json_string)
-    return data
-
 def render_to_tex(template_path, data):
+    """Render a jinja2 template to a tex string."""
     with open(template_path, 'r') as file:
         template = Template(file.read(),
                             lstrip_blocks=True, 
                             trim_blocks=True)
     return template.render(data)
 
-skills = open_json_file('data/skills.json')
-experience = open_json_file('data/experience.json')
-software = open_json_file('data/projects.json')
-education = open_json_file('data/education.json')
-writing = open_json_file('data/writing.json')
+def save_tex_file(file_path, tex_string):
+    """Save a string to a tex file."""
+    with open(file_path, 'w') as file:
+        file.write(tex_string)
+    return
 
+def process_data(file_path, template_path):
+    """Open a json file, render it to a tex string, and save it to a tex file."""
+    data = open_json_file(file_path)
+    output = render_to_tex(template_path, data={'data': data['content']})
+    save_tex_file(f'output/{file_path.split("/")[-1].split(".")[0]}.tex', output)
 
-skills_output = render_to_tex('j2/skills.tex.j2', data = {'data' : skills['content']})
-save_tex_file('output/skills.tex', skills_output)
-
-experience_output = render_to_tex('j2/experience.tex.j2', data = {'data' : experience['content']})
-save_tex_file('output/experience.tex', experience_output)
-
-software_output = render_to_tex('j2/software.tex.j2', data = {'data' : software['content']})
-save_tex_file('output/projects.tex', software_output)
-
-education_output = render_to_tex('j2/education.tex.j2', data = {'data' : education['content']})
-save_tex_file('output/education.tex', education_output)
-
-writing_output = render_to_tex('j2/writing.tex.j2', data = {'data' : writing['content']})
-save_tex_file('output/writing.tex', writing_output)
+process_data('data/skills.json', 'j2/skills.tex.j2')
+process_data('data/experience.json', 'j2/experience.tex.j2')
+process_data('data/projects.json', 'j2/software.tex.j2')
+process_data('data/education.json', 'j2/education.tex.j2')
+process_data('data/writing.json', 'j2/writing.tex.j2')
